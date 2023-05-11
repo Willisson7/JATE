e5604c34cda48f5451e502e5756fb2edf7c792cf
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-05-11 10:57:25 -0700

    Completed TODOs in database.js and Install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,32 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+      try {
+        await deferredPrompt.prompt();
+        const choiceResult = await deferredPrompt.userChoice;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+        if (choiceResult.outcome === 'accepted') {
+            console.log('PWA installed successfully!');
+          } else {
+            console.log('PWA installation declined.');
+          }
+          deferredPrompt = null;
+          butInstall.style.display = 'none';
+        } catch (error) {
+          console.error('Error prompting installation', error);
+        }
+    }
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA was installed.');
+});
